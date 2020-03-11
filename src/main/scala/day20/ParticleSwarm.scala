79900524dde711b58c6deec6ee1ab255bcda56b1
kelley.glenn <kelley.glenn@workday.com>
2020-03-10 17:42:57 -0700

    Day 20 - part 2

--- src/main/scala/day20/ParticleSwarm.scala
+++ src/main/scala/day20/ParticleSwarm.scala
@@ -1,40 +1,99 @@
 package day20
 
+import scala.util.matching.Regex.MatchIterator
+
 object ParticleSwarm {
   def slowestMovingIndex(lines: IterableOnce[String]): Int = {
-    val maxParticle = Particle(
+    val maxParticle: Particle = Particle(Short.MaxValue,
       Pos(Int.MaxValue, Int.MaxValue, Int.MaxValue),
       Pos(Int.MaxValue, Int.MaxValue, Int.MaxValue),
       Pos(Int.MaxValue, Int.MaxValue, Int.MaxValue))
-    parse(lines).zipWithIndex.toSet.fold((maxParticle, 0)) { case ((minP, minI), (p, i)) =>
-      if ((p.acceleration.distance < minP.acceleration.distance) ||
-        (p.acceleration.distance == minP.acceleration.distance && p.velocity.distance < minP.velocity.distance) ||
-        (p.acceleration.distance == minP.acceleration.distance &&
-          p.velocity.distance == minP.velocity.distance &&
-          p.pos.distance < minP.pos.distance)) (p, i)
-      else (minP, minI)
-    }._2
+    val slowestParticle: Particle = parse(lines).fold(maxParticle) { case (minP, p) =>
+      if ((p.acceleration.distance0 < minP.acceleration.distance0) ||
+        (p.acceleration.distance0 == minP.acceleration.distance0 && p.velocity.distance0 < minP.velocity.distance0) ||
+        (p.acceleration.distance0 == minP.acceleration.distance0 &&
+          p.velocity.distance0 == minP.velocity.distance0 &&
+          p.pos.distance0 < minP.pos.distance0)) p
+      else minP
+    }
+    slowestParticle.id
   }
 
-  def parse(lines: IterableOnce[String]): Iterator[Particle] = {
-    lines.iterator.map(parseParticle)
+  private def parse(lines: IterableOnce[String]): Iterable[Particle] = {
+    lines.iterator.zipWithIndex.map((si: (String, Int)) => parseParticle(si._1, si._2.toShort)).to(Iterable)
   }
 
-  def parseParticle(line: String): Particle = {
-    val particleGroups = raw"p=<(.*)>, v=<(.*)>, a=<(.*)>".r("p", "v", "a").findAllIn(line)
-    Particle(parsePos(particleGroups.group("p")), parsePos(particleGroups.group("v")), parsePos(particleGroups.group("a")))
+  private def parseParticle(line: String, id: Short): Particle = {
+    val particleGroups: MatchIterator = raw"p=<(.*)>, v=<(.*)>, a=<(.*)>".r("p", "v", "a").findAllIn(line)
+    Particle(id, parsePos(particleGroups.group("p")), parsePos(particleGroups.group("v")), parsePos(particleGroups.group("a")))
   }
 
-  def parsePos(s: String): Pos = {
-    val xyzGroups = raw"(-?\d+),(-?\d+),(-?\d+)".r("x", "y", "z").findAllIn(s)
+  private def parsePos(s: String): Pos = {
+    val xyzGroups: MatchIterator = raw"(-?\d+),(-?\d+),(-?\d+)".r("x", "y", "z").findAllIn(s)
     Pos(xyzGroups.group("x").toInt, xyzGroups.group("y").toInt, xyzGroups.group("z").toInt)
   }
 
-  case class Particle(pos: Pos, velocity: Pos, acceleration: Pos)
+  def afterAllCollisions(lines: IterableOnce[String]): Set[Particle] = {
+    var prevSurvivingParticles: Iterable[Particle] = parse(lines)
+    var prevDistances: Map[Short, Map[Short, Int]] = distances(prevSurvivingParticles)
+    var newSurvivingParticles: Iterable[Particle] = tick(prevSurvivingParticles)
+    var newDistances: Map[Short, Map[Short, Int]] = distances(newSurvivingParticles)
+    while (newSurvivingParticles.size < prevSurvivingParticles.size || anyCloser(newDistances, prevDistances)) {
+      prevSurvivingParticles = newSurvivingParticles
+      prevDistances = newDistances
+      newSurvivingParticles = tick(prevSurvivingParticles)
+      newDistances = distances(newSurvivingParticles)
+    }
+    prevSurvivingParticles.toSet
+  }
+
+  private def tick(particles: Iterable[Particle]): Iterable[Particle] = {
+    val newParticles: Iterable[Particle] = particles.map { case Particle(id, p, v, a) =>
+      val newV: Pos = v + a
+      Particle(id, Pos(p.x + newV.x, p.y + newV.y, p.z + newV.z), newV, a)
+    }
+    removeCollisions(newParticles)
+  }
+
+  private def anyCloser(newDistances: Map[Short, Map[Short, Int]], prevDistances: Map[Short, Map[Short, Int]]): Boolean = {
+    prevDistances.keySet.exists((id: Short) => anyCloserToParticle(newDistances(id), prevDistances(id)))
+  }
+
+  private def anyCloserToParticle(newDistances: Map[Short, Int], prevDistances: Map[Short, Int]): Boolean = {
+    prevDistances.keySet.exists((id: Short) => newDistances(id) < prevDistances(id))
+  }
+
+  private def removeCollisions(particles: Iterable[Particle]): Iterable[Particle] = {
+    val particlePosGroups: Map[Pos, Iterable[Particle]] = particles.groupBy(_.pos)
+    particles.filter((p: Particle) => particlePosGroups(p.pos).size == 1)
+  }
+
+  private def distances(particles: Iterable[Particle]): Map[Short, Map[Short, Int]] = {
+    if (particles.isEmpty) Map.empty
+    else distances(particles.tail) + (particles.head.id -> particles.head.distanceTo(particles.tail))
+  }
+
+  case class Particle(id: Short, pos: Pos, velocity: Pos, acceleration: Pos) {
+    def distanceTo(others: IterableOnce[Particle]): Map[Short, Int] = {
+      others.iterator.map((o: Particle) => (o.id, distanceTo(o))).toMap
+    }
+
+    def distanceTo(other: Particle): Int = {
+      pos.distanceTo(other.pos)
+    }
+  }
 
   case class Pos(x: Int, y: Int, z: Int) {
-    def distance: Int = {
+    def distance0: Int = {
       Math.abs(x) + Math.abs(y) + Math.abs(z)
+    }
+
+    def distanceTo(other: Pos): Int = {
+      Math.abs(x - other.x) + Math.abs(y - other.y) + Math.abs(z - other.z)
+    }
+
+    def +(other: Pos): Pos = {
+      Pos(x + other.x, y + other.y, z + other.z)
     }
   }
 
--- src/test/scala/day20/ParticleSwarmTest.scala
+++ src/test/scala/day20/ParticleSwarmTest.scala
@@ -5,10 +5,17 @@ import util.SetupPuzzleData
 
 class ParticleSwarmTest extends AnyFlatSpec {
   behavior of "slowestMovingIndex"
-  it should "handle the example" in new SetupPuzzleData("example") {
+  it should "handle the first example" in new SetupPuzzleData("example") {
     assert(ParticleSwarm.slowestMovingIndex(lines) == 0)
   }
-  it should "solve the puzzle" in new SetupPuzzleData("input") {
+  it should "solve the first puzzle" in new SetupPuzzleData("input") {
     assert(ParticleSwarm.slowestMovingIndex(lines) == 457)
+  }
+  behavior of "afterAllCollisions"
+  it should "handle the second example" in new SetupPuzzleData("example2") {
+    assert(ParticleSwarm.afterAllCollisions(lines).size == 1)
+  }
+  it should "solve the second puzzle" in new SetupPuzzleData("input") {
+    assert(ParticleSwarm.afterAllCollisions(lines).size == 448)
   }
 }
