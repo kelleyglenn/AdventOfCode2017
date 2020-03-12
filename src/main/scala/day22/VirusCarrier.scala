782cefdd31d310c878599a23ae8da017a9aa405f
kelley.glenn <kelley.glenn@workday.com>
2020-03-11 17:55:08 -0700

    Day 22 - part 2

--- src/main/scala/day22/VirusCarrier.scala
+++ src/main/scala/day22/VirusCarrier.scala
@@ -3,41 +3,73 @@ package day22
 import scala.collection.mutable
 
 object VirusCarrier {
-  def countBurstsCausingInfection(startingMap: Seq[String], burstCount: Int): Int = {
-    val grid: mutable.Map[Pos, Boolean] = parse(startingMap)
+  def countBurstsCausingInfection[A](startingMap: Seq[String], burstCount: Int, nodeInfo: NodeStateOps[A]): Int = {
+    val grid: mutable.Map[Pos, A] = parse(startingMap, nodeInfo)
     var infectionCt = 0
-    var curPos = Pos(0, 0)
-    var curDirection = Pos(0, -1)
-
-    def turn(right: Boolean): Unit = {
-      val rightVal = if (right) 1 else -1
-      curDirection = if (curDirection.x == 0) {
-        Pos(if (curDirection.y == 1) -rightVal else rightVal, 0)
-      } else {
-        Pos(0, if (curDirection.x == 1) rightVal else -rightVal)
-      }
-    }
-
-    (1 to burstCount).foreach { _ =>
-      val curInfected = grid(curPos)
-      turn(curInfected)
-      if (!curInfected) infectionCt += 1
-      grid(curPos) = !curInfected
+    var curPos: Pos = Pos(0, 0)
+    var curDirection: Pos = Pos(0, -1)
+    (1 to burstCount).foreach { _: Int =>
+      var curState: A = grid(curPos)
+      curDirection = nodeInfo.turn(curDirection, curState)
+      curState = nodeInfo.nextState(curState)
+      grid(curPos) = curState
+      if (curState == nodeInfo.infectedState) infectionCt += 1
       curPos = curPos + curDirection
     }
     infectionCt
   }
 
-  private def parse(startingMap: Seq[String]): mutable.Map[Pos, Boolean] = {
-    val middleY = (startingMap.size - 1) / 2
-    val middleX = (startingMap.head.length - 1) / 2
-    val map: mutable.Map[Pos, Boolean] = mutable.Map.empty.withDefaultValue(false)
-    startingMap.indices.foreach { y =>
-      startingMap(y).indices.foreach { x =>
-        if (startingMap(y)(x) == '#') map.addOne(Pos(x - middleX, y - middleY) -> true)
+  private def parse[B](startingMap: Seq[String], nodeInfo: NodeStateOps[B]): mutable.Map[Pos, B] = {
+    val middleY: Int = (startingMap.size - 1) / 2
+    val middleX: Int = (startingMap.head.length - 1) / 2
+    val map: mutable.Map[Pos, B] = mutable.Map.empty.withDefaultValue(nodeInfo.cleanState)
+    startingMap.indices.foreach { y: Int =>
+      startingMap(y).indices.foreach { x: Int =>
+        if (startingMap(y)(x) == '#') map.addOne(Pos(x - middleX, y - middleY) -> nodeInfo.infectedState)
       }
     }
     map
+  }
+
+  trait NodeStateOps[NS] {
+    val infectedState: NS
+    val cleanState: NS
+
+    val nextState: Map[NS, NS]
+
+    def turn(curDirection: Pos, curState: NS): Pos
+  }
+
+  object Part1Ops extends NodeStateOps[Boolean] {
+    override val cleanState = false
+    override val infectedState = true
+    override val nextState: Map[Boolean, Boolean] =
+      Map(cleanState -> infectedState, infectedState -> cleanState)
+
+    override def turn(curDirection: Pos, curState: Boolean): Pos = {
+      val rightVal: Int = if (curState == infectedState) 1 else -1
+      if (curDirection.x == 0) Pos(if (curDirection.y == 1) -rightVal else rightVal, 0)
+      else Pos(0, if (curDirection.x == 1) rightVal else -rightVal)
+    }
+  }
+
+  object Part2States extends Enumeration {
+    val Clean, Weakened, Infected, Flagged = Value
+  }
+
+  object Part2Ops extends NodeStateOps[Part2States.Value] {
+    override val cleanState: Part2States.Value = Part2States.Clean
+    override val infectedState: Part2States.Value = Part2States.Infected
+    override val nextState: Map[Part2States.Value, Part2States.Value] =
+      Map(Part2States.Clean -> Part2States.Weakened, Part2States.Weakened -> Part2States.Infected,
+        Part2States.Infected -> Part2States.Flagged, Part2States.Flagged -> Part2States.Clean)
+
+    override def turn(curDirection: Pos, curState: Part2States.Value): Pos = {
+      if (curState == Part2States.Infected || curState == Part2States.Clean)
+        Part1Ops.turn(curDirection, curState == Part2States.Infected)
+      else if (curState == Part2States.Flagged) Pos(-curDirection.x, -curDirection.y)
+      else curDirection
+    }
   }
 
   case class Pos(x: Int, y: Int) {
--- src/test/scala/day22/VirusCarrierTest.scala
+++ src/test/scala/day22/VirusCarrierTest.scala
@@ -5,13 +5,21 @@ import util.SetupPuzzleData
 
 class VirusCarrierTest extends AnyFlatSpec {
   behavior of "countBurstsCausingInfection"
-  it should "handle the example" in {
+  it should "handle the first example" in {
     val map = List("..#", "#..", "...")
-    assert(VirusCarrier.countBurstsCausingInfection(map, 7) == 5)
-    assert(VirusCarrier.countBurstsCausingInfection(map, 70) == 41)
-    assert(VirusCarrier.countBurstsCausingInfection(map, 10000) == 5587)
+    assert(VirusCarrier.countBurstsCausingInfection(map, 7, VirusCarrier.Part1Ops) == 5)
+    assert(VirusCarrier.countBurstsCausingInfection(map, 70, VirusCarrier.Part1Ops) == 41)
+    assert(VirusCarrier.countBurstsCausingInfection(map, 10000, VirusCarrier.Part1Ops) == 5587)
   }
-  it should "solve the puzzle" in new SetupPuzzleData("input") {
-    assert(VirusCarrier.countBurstsCausingInfection(lines, 10000) == 5450)
+  it should "solve the first puzzle" in new SetupPuzzleData("input") {
+    assert(VirusCarrier.countBurstsCausingInfection(lines, 10000, VirusCarrier.Part1Ops) == 5450)
+  }
+  it should "handle the second example" in {
+    val map = List("..#", "#..", "...")
+    assert(VirusCarrier.countBurstsCausingInfection(map, 100, VirusCarrier.Part2Ops) == 26)
+    assert(VirusCarrier.countBurstsCausingInfection(map, 10000000, VirusCarrier.Part2Ops) == 2511944)
+  }
+  it should "solve the second puzzle" in new SetupPuzzleData("input") {
+    assert(VirusCarrier.countBurstsCausingInfection(lines, 10000000, VirusCarrier.Part2Ops) == 2511957)
   }
 }
